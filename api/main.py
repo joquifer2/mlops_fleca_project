b0dc4af2933914ff860ce37a03595d524b9478fc
Jordi <jordi@jordiquiroga.com>
2025-09-05 16:54:21 +0200

    Actualiza y corrige la lógica de la API: routers, schemas y main

---
+++ api/main.py
@@ -0,0 +1,17 @@
+# Su misión es ser el punto de entrada principal para la aplicación API.
+# Crea la instancia principal de FastAPI y une todas las piezas (como los diferentes routers).
+
+from fastapi import FastAPI
+from api.routers import router
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Crea una instancia de FastAPI
+app = FastAPI(
+    title="Bolleria Predicction API", 
+    description="API para predecir las ventas de bollería usando modelo entrenado", 
+    version="1.0.0"
+)
+
+# Incluye el router de la aplicación FastAPI
+app.include_router(router)
--- api/schemas.py
+++ api/schemas.py
@@ -1,10 +1,25 @@
-from pydantic import BaseModel
+# Su única misión es definir la "forma" de tus datos
+# Contiene todos tus modelos Pydantic para las solicitudes y respuestas de la API
+# Si necesitas saber qué datos entran o salen de la API, este es el archivo a revisar
+
+
+from pydantic import BaseModel # Importa BaseModel de Pydantic para definir modelos de datos
 from datetime import date
 
-class PredictionRequest(BaseModel):
-    # Define aquí los campos de entrada necesarios para la predicción
-    pass
 
+# Define los modelos de datos que se utilizarán en la aAPI
+class PredictionRequest(BaseModel):
+    """
+    Modelo de datos para la solicitud de predicción.
+    Actualmente no se requieren parámetros en la solicitud.
+    """
+    timestamp: str  # formato ISO, e.g., "2023-10-01T00:00:00"
+
+
+# Define un modelo para la predicción
 class PredictionResponse(BaseModel):
+    """
+    Modelo de datos para la respuesta de predicción.
+    """
     week_start: date
     prediction: float
