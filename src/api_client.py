1222a2bb5a9fb64558a1d493ac2d88cc9d154028
Jordi <jordi@jordiquiroga.com>
2025-09-23 16:48:33 +0200

    Actualización de api_client.py: ajuste de endpoint y mejoras

--- src/api_client.py
+++ src/api_client.py
@@ -2,9 +2,9 @@ import requests
 import pandas as pd
 import os
 
-# API_URL = "http://127.0.0.1:8000/predict"  # Puedes parametrizar esto si lo necesitas
+API_URL = "http://127.0.0.1:8080/predict"  # Puedes parametrizar esto si lo necesitas
 DEFAULT_API_URL = "http://fastapi:8000/predict"  # Usar el nombre del servicio Docker
-API_URL = os.getenv("API_URL", DEFAULT_API_URL)
+#API_URL = os.getenv("API_URL", DEFAULT_API_URL)
 
 def llamar_api_prediccion(timestamp, api_url=API_URL):
     """
